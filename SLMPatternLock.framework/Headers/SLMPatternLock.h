b00d778f659ade855c715fff177cfd8e8bd0a710
myxc <70953073@qq.com>
2018-10-27 19:23:23 +0800

    cmmit new

---
+++ SLMPatternLock.framework/Headers/SLMPatternLock.h
@@ -0,0 +1,20 @@
+//
+//  SLMPatternLock.h
+//  SLMPatternLock
+//
+//  Created by myxc on 2018/10/27.
+//  Copyright © 2018 myxc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SLMPatternLock.
+FOUNDATION_EXPORT double SLMPatternLockVersionNumber;
+
+#import "SLMPatternLock/SLMPatternLockManager.h"
+
+FOUNDATION_EXPORT const unsigned char SLMPatternLockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SLMPatternLock/PublicHeader.h>
+
+
